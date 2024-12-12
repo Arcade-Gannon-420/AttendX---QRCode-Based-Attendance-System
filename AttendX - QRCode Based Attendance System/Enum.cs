5f4fee80dfa4daf2bd5d57de7c021233e465a17f
ERP-JOHNPAULRMIAGA <ERP-JOHNPAULRMIAGA@DESKTOP-O2556K6>
2024-12-12 10:57:13 +0800

    Add project files.

---
+++ AttendX - QRCode Based Attendance System/Enum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendX___QRCode_Based_Attendance_System
+{
+    internal class Enum
+    {
+    }
+    public static class UserStatus
+    {
+        public const string Active = "A";
+        public const string Inactive = "I";
+        public static string Display(this string value)
+        {
+            return value == "A" ? "Active"
+                  : value == "I" ? "Inactive"
+                  : string.Empty;
+        }
+    }
+    public static class EnrollmentStatus
+    {
+        public const string Enrolled = "E";
+        public const string Pending = "P";
+        public const string NotEnrolled = "N";
+        public static string Display(this string value)
+        {
+            return value == "E" ? "Enrolled"
+                  : value == "P" ? "Pending"
+                  : value == "N" ? "NotEnrolled"
+                  : string.Empty;
+        }
+    }
+}
