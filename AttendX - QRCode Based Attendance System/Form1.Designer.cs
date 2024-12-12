5ae35e9163e099a956a46abb4f8977ad5de94ae0
Arcade-Gannon-420 <bgmiaga1999@gmail.com>
2024-12-12 16:42:38 +0800

    v1.0.0

--- AttendX - QRCode Based Attendance System/Form1.Designer.cs
+++ AttendX - QRCode Based Attendance System/Form1.Designer.cs
@@ -28,27 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.CameraBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.03752F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.96248F));
+            this.tableLayoutPanel1.Controls.Add(this.CameraBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 596F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 619F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1226, 726);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.CameraBox.ErrorImage = null;
+            this.CameraBox.InitialImage = null;
+            this.CameraBox.Location = new System.Drawing.Point(972, 473);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(250, 250);
+            this.CameraBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CameraBox.TabIndex = 1;
+            this.CameraBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 107);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AttendX";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoSize = true;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1250, 750);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -57,6 +88,11 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -65,6 +101,8 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox CameraBox;
     }
 }
 
--- AttendX - QRCode Based Attendance System/Form1.cs
+++ AttendX - QRCode Based Attendance System/Form1.cs
@@ -8,17 +8,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using System.Threading;
 
 namespace AttendX___QRCode_Based_Attendance_System
 {
     public partial class Form1 : Form
     {
         public AttendXEntities db = new AttendXEntities();
+
+        VideoCapture _capture = new VideoCapture(0);
+        private Mat _image = new Mat();
+        Thread thread;
+
         public Form1()
         {
             InitializeComponent();
-        }
-        
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            thread = new Thread(new ThreadStart(CaptureCameraCallback));
+            thread.Start();
+        }
+
+        public void CaptureCameraCallback()
+        {
+            while (true)
+            {
+                _capture.Read(_image);
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(() => CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image)));
+                }
+                else
+                {
+                    CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+        }
     }
 }
