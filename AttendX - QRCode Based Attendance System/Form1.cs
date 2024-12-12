866445e289f024084390046e5cae09a9324d9500
Arcade-Gannon-420 <bgmiaga1999@gmail.com>
2024-12-12 17:14:48 +0800

    v.1.0.0

--- AttendX - QRCode Based Attendance System/Form1.cs
+++ AttendX - QRCode Based Attendance System/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using AttendX___QRCode_Based_Attendance_System.Model;
+using AttendX___QRCode_Based_Attendance_System.Common.Helper;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,41 +18,91 @@ namespace AttendX___QRCode_Based_Attendance_System
     {
         public AttendXEntities db = new AttendXEntities();
 
-        VideoCapture _capture = new VideoCapture(0);
+        private VideoCapture _capture;
         private Mat _image = new Mat();
-        Thread thread;
+        private bool _isRunning = true;
 
         public Form1()
         {
             InitializeComponent();
-
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
-            thread = new Thread(new ThreadStart(CaptureCameraCallback));
-            thread.Start();
-        }
-
-        public void CaptureCameraCallback()
-        {
-            while (true)
+            await InitializeCameraAsync();
+            if (_capture != null && _capture.IsOpened())
             {
-                _capture.Read(_image);
-                if (this.InvokeRequired)
-                {
-                    this.Invoke(new MethodInvoker(() => CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image)));
-                }
-                else
-                {
-                    CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image);
-                }
+                await StartCameraFeedAsync();
             }
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+            while (_capture == null || !_capture.IsOpened())
+            {
+                try
+                {
+                    _capture = new VideoCapture(0); // 0 for default camera
+                    if (_capture.IsOpened())
+                    {
+                        //MessageBox.Show("Camera successfully started!");
+                        AutoClosingMessageBox.Show("Camera successfully started!", "", 3000);
+                        break;
+                    }
+                    else
+                    {
+                        _capture.Release();
+                        _capture.Dispose();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Optionally log the exception
+                }
+                AutoClosingMessageBox.Show("Unable to access the camera. Retrying in 3 seconds...", "", 3000);
+                
+            }
+        }
+
+        private async Task StartCameraFeedAsync()
+        {
+            await Task.Run(() =>
+            {
+                while (_isRunning)
+                {
+                    _capture.Read(_image);
+                    if (_image.Empty())
+                        continue;
+
+                    if (CameraBox.InvokeRequired)
+                    {
+                        CameraBox.Invoke(new MethodInvoker(() =>
+                        {
+                            CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image);
+                        }));
+                    }
+                    else
+                    {
+                        CameraBox.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(_image);
+                    }
+                }
+            });
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            thread.Abort();
+            _isRunning = false;
+
+            if (_capture != null)
+            {
+                _capture.Release();
+                _capture.Dispose();
+            }
+
+            if (_image != null)
+            {
+                _image.Dispose();
+            }
         }
     }
 }
